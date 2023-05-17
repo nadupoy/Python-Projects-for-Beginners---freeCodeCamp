85301238cd967a6bffdc6f2dd8f84b20334e8c9c
Nadupoy <gnsampao@gmail.com>
2023-05-17 19:21:39 +0300

    Increased the number range from 3 to 5.

--- numberGame-user.py
+++ numberGame-user.py
@@ -1,14 +1,14 @@
 import random
 
-number_01 = random.randint(1, 3)
-number_02 = input("Guess a number. ")
+number_01 = random.randint(1, 5)
+number_02 = input("Guess a number between 1 and 5. ")
 number_02 = int(number_02)
 
 while number_01 != number_02:
     print(number_01, number_02)
     print("The numbers are not a match. 😔\n")
-    number_01 = random.randint(1, 3)
-    number_02 = input("Guess a number. ")
+    number_01 = random.randint(1, 5)
+    number_02 = input("Guess a number between 1 and 5. ")
     number_02 = int(number_02)
 else:
     print(number_01, number_02)
